7e31dbc1567a27142dff5182dc018d3f87cf2613
Anastasiya Karpenka <knesty@yandex.by>
2023-10-17 16:27:08 +0300

    added responsive header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,48 @@
 /* eslint-disable react/prop-types */
 import './header.scss';
 import { HashLink as Link } from 'react-router-hash-link';
+import { useState } from 'react';
 
-const Header = () => (
-    <header>
-        <p>Anastasiya Karpenka</p>
-        <nav className="nav-btns">
-            <Link to="/#about">about</Link>
-            <Link to="/#portfolio">portfolio</Link>
-            <Link to="/#experience">experience</Link>
-            <Link to="/#education">education</Link>
-            <Link to="/#certificates">certificates</Link>
-        </nav>
-        <Link to="/#contacts" className="black-btn">contacts</Link>
-    </header>
-);
+const Header = () => {
+    const [menuBarClass, setMenuBarClass] = useState('');
+    const [navClass, setNavlass] = useState('');
+    const [menuBGClass, setMenuBGClass] = useState('');
+
+    const menuOnClick = () => {
+        setMenuBarClass(menuBarClass ? '' : 'change');
+        setNavlass(navClass ? '' : 'change');
+        setMenuBGClass(menuBGClass ? '' : 'change-bg');
+    };
+
+    const renderMenu = (size) => {
+        return (
+            <nav className={`nav-btns ${size} ${navClass}`} id="nav">
+                <Link to="/#about">about</Link>
+                <Link to="/#portfolio">portfolio</Link>
+                <Link to="/#experience">experience</Link>
+                <Link to="/#education">education</Link>
+                <Link to="/#certificates">certificates</Link>
+            </nav>
+        )
+    };
+
+    return (
+        <header>
+            <div id="menuBurger">
+                <div id="menuBar" onClick={menuOnClick} className={menuBarClass}>
+                    <div id="bar1" className="bar"></div>
+                    <div id="bar2" className="bar"></div>
+                    <div id="bar3" className="bar"></div>
+                </div>
+                {renderMenu('')}
+                <div className={`menu-bg ${menuBGClass}`}></div>
+            </div>
+            <p>Anastasiya Karpenka</p>
+            {renderMenu('lg')}
+            <Link to="/#contacts" className="black-btn">contacts</Link>
+        </header>
+    )
+        
+};
 
 export default Header;
